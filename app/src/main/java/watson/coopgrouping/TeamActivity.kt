81c2d70df051def5c1168b362de24bebc73be128
watsonyao <yaoyuan131617@gmail.com>
2024-11-09 10:52:57 +0800

    f

--- app/src/main/java/watson/coopgrouping/TeamActivity.kt
+++ app/src/main/java/watson/coopgrouping/TeamActivity.kt
@@ -37,13 +37,13 @@ class TeamActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
     //w5
     findViewById<Button>(R.id.w5_240).setOnClickListener {
-      runTTs(bossW5_240)
+      runTTs(bossW5_240, true)
     }
     findViewById<Button>(R.id.w5_270).setOnClickListener {
-      runTTs(bossW5_270)
+      runTTs(bossW5_270, true)
     }
     findViewById<Button>(R.id.w5_300).setOnClickListener {
-      runTTs(bossW5_300)
+      runTTs(bossW5_300, true)
     }
 
 
@@ -62,29 +62,40 @@ class TeamActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     clock?.text = "100"
   }
 
-  private fun checkBoss(index: Int, boss: List<Thing>) {
+  private fun checkBoss(index: Int, boss: List<Thing>, change: Boolean) {
     val find = boss.filter { it.index == index }
     if (find.size > 0) {
-      speak(find)
+      speak(find, change)
     }
   }
 
-  private fun speak(list: List<Thing>) {
+  private fun speak(list: List<Thing>, change: Boolean) {
     val items = list.map {
-      "${it.position}${it.name}"
+      val position = if (change) {
+        if (it.position == A) {
+          C
+        } else if (it.position == C) {
+          A 
+        } else {
+          it.position
+        }
+      } else {
+        it.position
+      }
+      "${position}${it.name}"
     }
     val text = items.joinToString(separator = ",")
     tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
   }
 
-  private fun runTTs(boss: List<Thing>) {
+  private fun runTTs(boss: List<Thing>, change: Boolean = false) {
     closeTTs()
     countdown = 100
     timer = object : CountDownTimer(100000, 1000) {
       override fun onTick(millisUntilFinished: Long) {
         if (countdown >= 0) {
           clock?.text = "$countdown"
-          checkBoss(countdown, boss)
+          checkBoss(countdown, boss, change)
           countdown--
         }
       }
@@ -177,100 +188,100 @@ class TeamActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
   private val bossW5_240 by lazy {
     listOf(
-      Thing(100, "锅盖", B),
-      Thing(96, "伞", B),
-      Thing(94, "铁", B),
+      Thing(100, "锅盖", C),
+      Thing(96, "伞", C),
+      Thing(94, "铁", C),
       Thing(89, "绿帽", A),
       Thing(89, "锅盖", A),
       Thing(84, "柱子", A),
       Thing(79, "铁", C),
-      Thing(76, "两塔", C),
-      Thing(72, "车", C),
-      Thing(69, "塔", B),
-      Thing(68, "铁", B),
-      Thing(63, "鼹鼠", B),
-      Thing(60, "铁", B),
-      Thing(58, "伞", C),
-      Thing(56, "垫肩", C),
+      Thing(76, "两塔", B),
+      Thing(72, "车", B),
+      Thing(69, "塔", C),
+      Thing(68, "铁", C),
+      Thing(63, "鼹鼠", C),
+      Thing(60, "铁", C),
+      Thing(58, "伞", B),
+      Thing(56, "垫肩", B),
       Thing(48, "铁", C),
-      Thing(48, "蛇", C),
+      Thing(48, "蛇", B),
       Thing(48, "车", A),
       Thing(47, "鼹鼠", A),
       Thing(38, "鼹鼠", A),
       Thing(38, "绿帽", A),
-      Thing(38, "塔", C),
-      Thing(34, "鼹鼠", C),
-      Thing(31, "垫肩", C),
-      Thing(31, "海豚", C),
+      Thing(38, "塔", B),
+      Thing(34, "鼹鼠", B),
+      Thing(31, "垫肩", B),
+      Thing(31, "海豚", B),
       Thing(28, "鼹鼠", A),
 
       )
   }
   private val bossW5_270 by lazy {
     listOf(
-      Thing(100, "锅盖", B),
-      Thing(99, "伞", B),
-      Thing(94, "铁", B),
+      Thing(100, "锅盖", C),
+      Thing(99, "伞", C),
+      Thing(94, "铁", C),
       Thing(91, "绿帽", A),
       Thing(88, "锅盖", A),
       Thing(87, "柱子", A),
-      Thing(82, "铁", C),
-      Thing(77, "两塔", C),
+      Thing(82, "铁", B),
+      Thing(77, "两塔", B),
       Thing(73, "垫肩", A),
       Thing(72, "鼹鼠", A),
       Thing(66, "铁", A),
-      Thing(64, "塔", B),
-      Thing(64, "铁", B),
-      Thing(61, "柱子", B),
-      Thing(56, "蛇", B),
-      Thing(55, "铁", C),
-      Thing(54, "蛇", C),
-      Thing(52, "蛇", C),
-      Thing(46, "伞", C),
-      Thing(46, "鼹鼠", B),
-      Thing(40, "绿帽", B),
-      Thing(37, "两鼹鼠", B),
-      Thing(37, "绿帽", C),
-      Thing(34, "锅盖", C),
-      Thing(31, "塔", C),
-      Thing(28, "垫肩", C),
+      Thing(64, "塔", C),
+      Thing(64, "铁", C),
+      Thing(61, "柱子", C),
+      Thing(56, "蛇", C),
+      Thing(55, "铁", B),
+      Thing(54, "蛇", B),
+      Thing(52, "蛇", B),
+      Thing(46, "伞", B),
+      Thing(46, "鼹鼠", C),
+      Thing(40, "绿帽", C),
+      Thing(37, "两鼹鼠", C),
+      Thing(37, "绿帽", B),
+      Thing(34, "锅盖", A),
+      Thing(31, "塔", B),
+      Thing(28, "垫肩", B),
       Thing(28, "车", A),
     )
   }
 
   private val bossW5_300 by lazy {
     listOf(
-      Thing(100, "锅盖", B),
-      Thing(99, "伞", B),
-      Thing(94, "铁", B),
+      Thing(100, "锅盖", C),
+      Thing(99, "伞", C),
+      Thing(94, "铁", C),
       Thing(91, "绿帽", A),
       Thing(88, "锅盖", A),
       Thing(87, "柱子", A),
       Thing(84, "锅盖", A),
-      Thing(82, "蛇", C),
-      Thing(78, "车", C),
-      Thing(77, "绿帽", C),
-      Thing(77, "车", C),
-      Thing(73, "铁", B),
-      Thing(72, "两鼹鼠", B),
-      Thing(66, "车", B),
-      Thing(64, "绿帽", C),
-      Thing(64, "柱子", C),
-      Thing(61, "伞", C),
-      Thing(56, "蛇", C),
+      Thing(82, "蛇", B),
+      Thing(78, "车", B),
+      Thing(77, "绿帽", B),
+      Thing(77, "车", B),
+      Thing(73, "铁", C),
+      Thing(72, "两鼹鼠", C),
+      Thing(66, "车", C),
+      Thing(64, "绿帽", B),
+      Thing(64, "柱子", B),
+      Thing(61, "伞", B),
+      Thing(56, "蛇", B),
       Thing(55, "车", A),
       Thing(54, "绿帽", A),
-      Thing(52, "锅盖", A),
-      Thing(46, "绿帽", A),
-      Thing(46, "垫肩", C),
-      Thing(40, "鼹鼠", C),
-      Thing(37, "鼹鼠", C),
-      Thing(37, "蛇", C),
-      Thing(37, "绿帽", B),
-      Thing(34, "垫肩", B),
-      Thing(31, "鼹鼠", B),
-      Thing(28, "锅盖", B),
-      Thing(28, "鼹鼠", C),
+      Thing(52, "锅盖", C),
+      Thing(46, "绿帽", C),
+      Thing(46, "垫肩", B),
+      Thing(40, "鼹鼠", B),
+      Thing(37, "鼹鼠", B),
+      Thing(37, "蛇", B),
+      Thing(37, "绿帽", C),
+      Thing(34, "垫肩", C),
+      Thing(31, "鼹鼠", C),
+      Thing(28, "锅盖", C),
+      Thing(28, "鼹鼠", B),
     )
   }
 
