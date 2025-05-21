d2b34e04f35c6e4329b9ae5265cd6e6bedafb46d
watsonyao <yaoyuan131617@gmail.com>
2025-05-21 14:36:09 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "watson.coopgrouping"
     minSdk = 26
     targetSdk = 34
-    versionCode = 11
-    versionName = "1.4.1"
+    versionCode = 12
+    versionName = "1.4.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
--- app/src/main/java/watson/coopgrouping/ApiService.kt
+++ app/src/main/java/watson/coopgrouping/ApiService.kt
@@ -1,15 +1,9 @@
 package watson.coopgrouping
 
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
-import watson.coopgrouping.JSON2
-import watson.coopgrouping.JSON3
 
 interface ApiService {
-
-    @GET("sample")
-    fun getSampleData(): Call<ResponseBody>
 
     @GET("https://splatoon2.ink/data/coop-schedules.json")
     fun getSplatoon2Schedules(): Call<JSON2>
